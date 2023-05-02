9036140132ed477d6c33f51e8779b2fd94ec9ba9
okkez <okkez000@gmail.com>
2023-05-02 16:09:29 +0900

    Add models

---
+++ internal/provider/models.go
@@ -0,0 +1,22 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+)
+
+type UserModel struct {
+	Name types.String `tfsdk:"name"`
+	Host types.String `tfsdk:"host"`
+}
+
+type RoleModel struct {
+	Name types.String `tfsdk:"name"`
+	Host types.String `tfsdk:"host"`
+}
+
+var RoleTypes = map[string]attr.Type{
+	"name": types.StringType,
+	"host": types.StringType,
+}
