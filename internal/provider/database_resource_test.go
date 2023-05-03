d9776a6eebf6fcc1bef9d6e96284efbad1b905e0
okkez <okkez000@gmail.com>
2023-05-04 08:43:50 +0900

    Add CheckDestroy

--- internal/provider/database_resource_test.go
+++ internal/provider/database_resource_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 func TestAccDatabaseResource(t *testing.T) {
@@ -13,6 +14,7 @@ func TestAccDatabaseResource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy: testAccDatabaseResource_CheckDestroy(name),
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
@@ -25,16 +27,18 @@ func TestAccDatabaseResource(t *testing.T) {
 				),
 			},
 			// ImportState testing
-			// {
-			// 	ResourceName:      "mysql_database.test",
-			// 	ImportState:       true,
-			// 	ImportStateVerify: true,
-			// },
+			{
+				ResourceName:      "mysql_database.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
 			// Update and Read testing
 			{
-				Config: testAccDatabaseResourceConfig(name),
+				Config: testAccDatabaseResourceConfig_full(name, "latin1", "latin1_bin"),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("mysql_database.test", "name", name),
+					resource.TestCheckResourceAttr("mysql_database.test", "default_character_set", "latin1"),
+					resource.TestCheckResourceAttr("mysql_database.test", "default_collation", "latin1_bin"),
 				),
 			},
 			// Delete testing automatically occurs in TestCase
@@ -45,8 +49,37 @@ func TestAccDatabaseResource(t *testing.T) {
 func testAccDatabaseResourceConfig(name string) string {
 	config := fmt.Sprintf(`
 resource "mysql_database" "test" {
-  name = "%s"
+  name = %q
 }
 `, name)
 	return buildConfig(config)
+}
+
+func testAccDatabaseResourceConfig_full(name, charset, collation string) string {
+	config := fmt.Sprintf(`
+resource "mysql_database" "test" {
+  name = %q
+  default_character_set = %q
+  default_collation = %q
+}
+`, name, charset, collation)
+	return buildConfig(config)
+}
+
+func testAccDatabaseResource_CheckDestroy(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		db := testDatabase()
+		sql := `SELECT count(*) FROM information_schema.schemata WHERE SCHEMA_NAME = ?`
+		args := []interface{}{name}
+		var count string
+		if err := db.QueryRow(sql, args...).Scan(&count); err != nil {
+			return err
+		}
+
+		if count == "0" {
+			return nil
+		} else {
+			return fmt.Errorf("database still exists after descroy (%s)", count)
+		}
+	}
 }
