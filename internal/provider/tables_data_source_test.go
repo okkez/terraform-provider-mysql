d1055e4d1e0fe65c1bea1a7f88749ac7a7ca23eb
okkez <okkez000@gmail.com>
2023-05-04 09:49:22 +0900

    Fix test for mysql_tables data source

--- internal/provider/tables_data_source_test.go
+++ internal/provider/tables_data_source_test.go
@@ -1,48 +1,66 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccTablesDataSource(t *testing.T) {
+	database := "mysql"
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Read testing
 			{
-				Config: buildConfig(`
-data "mysql_tables" "test" {
-  database = "test"
-}
-`),
+				Config: testAccTablesDataSource_config(database),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.mysql_tables.test", "id", "test:"),
-					resource.TestCheckResourceAttr("data.mysql_tables.test", "database", "test"),
+					resource.TestCheckResourceAttr("data.mysql_tables.test", "id", "mysql:"),
+					resource.TestCheckResourceAttr("data.mysql_tables.test", "database", database),
 					resource.TestCheckNoResourceAttr("data.mysql_tables.test", "pattern"),
-					resource.TestCheckResourceAttr("data.mysql_tables.test", "tables.0", "test"),
-					resource.TestCheckResourceAttr("data.mysql_tables.test", "tables.1", "test2"),
-					resource.TestCheckResourceAttr("data.mysql_tables.test", "tables.2", "test3"),
-					resource.TestCheckNoResourceAttr("data.mysql_tables.test", "tables.3"),
+					resource.TestCheckResourceAttr("data.mysql_tables.test", "tables.#", testAccTablesDataSource_tableCount(t, database, "%")),
 				),
 			},
 			{
-				Config: buildConfig(`
-data "mysql_tables" "test" {
-  database = "test"
-  pattern  = "%2"
-}
-`),
+				Config: testAccTablesDataSource_configWithPattern(database, "time%"),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.mysql_tables.test", "id", "test:%2"),
-					resource.TestCheckResourceAttr("data.mysql_tables.test", "database", "test"),
-					resource.TestCheckResourceAttr("data.mysql_tables.test", "pattern", "%2"),
-					resource.TestCheckResourceAttr("data.mysql_tables.test", "tables.0", "test2"),
-					resource.TestCheckNoResourceAttr("data.mysql_tables.test", "tables.1"),
+					resource.TestCheckResourceAttr("data.mysql_tables.test", "id", "mysql:time%"),
+					resource.TestCheckResourceAttr("data.mysql_tables.test", "database", database),
+					resource.TestCheckResourceAttr("data.mysql_tables.test", "pattern", "time%"),
+					resource.TestCheckResourceAttr("data.mysql_tables.test", "tables.#", testAccTablesDataSource_tableCount(t, database, "time%")),
 				),
 			},
 		},
 	})
+}
+
+func testAccTablesDataSource_config(database string) string {
+	config := fmt.Sprintf(`
+data "mysql_tables" "test" {
+  database = %q
+}
+`, database)
+	return buildConfig(config)
+}
+
+func testAccTablesDataSource_configWithPattern(database, pattern string) string {
+	config := fmt.Sprintf(`
+data "mysql_tables" "test" {
+  database = %q
+  pattern = %q
+}
+`, database, pattern)
+	return buildConfig(config)
+}
+
+func testAccTablesDataSource_tableCount(t *testing.T, database, pattern string) string {
+	db := testDatabase()
+	sql := "SELECT COUNT(*) FROM information_schema.tables WHERE TABLE_SCHEMA = ? AND TABLE_NAME LIKE ?"
+	var count string
+	if err := db.QueryRow(sql, database, pattern).Scan(&count); err != nil {
+		t.Fatalf("Failed querying table count (%s %s): %s", database, pattern, err.Error())
+	}
+	return count
 }
