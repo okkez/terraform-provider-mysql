434181d984d43f2558200317eea687043ee4f388
okkez <okkez000@gmail.com>
2023-05-04 12:21:36 +0900

    Add utility methods to UserModel

--- internal/provider/models.go
+++ internal/provider/models.go
@@ -12,6 +12,26 @@ type UserModel struct {
 	Host types.String `tfsdk:"host"`
 }
 
+func NewUser(name, host string) UserModel {
+	return UserModel{
+		Name: types.StringValue(name),
+		Host: types.StringValue(host),
+	}
+}
+
+func (u *UserModel) GetName() string {
+	return u.Name.ValueString()
+}
+
+func (u *UserModel) GetHost() string {
+	return u.Host.ValueString()
+}
+
+func (u *UserModel) GetID() string {
+	return fmt.Sprintf("%s@%s", u.GetName(), u.GetHost())
+}
+
+
 type RoleModel struct {
 	Name types.String `tfsdk:"name"`
 	Host types.String `tfsdk:"host"`
