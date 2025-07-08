e0a57ab94db2067adf404f4bdc99fe30430e92da
okkez <okkez000@gmail.com>
2025-07-09 05:28:14 +0900

    feat: support MySQL8.4 use caching_sha2_password by default

--- internal/provider/user_resource.go
+++ internal/provider/user_resource.go
@@ -252,13 +252,20 @@ WHERE
 		data.Lock = types.BoolValue(accountLocked == "Y")
 
 		if data.AuthOption.IsNull() {
+			// https://dev.mysql.com/doc/refman/8.4/en/native-pluggable-authentication.html
+			// The mysql_native_password authentication plugin is deprecated as of MySQL 8.0.34, disabled by default in MySQL 8.4,
+			// and removed as of MySQL 9.0.0.
 			// See https://dev.mysql.com/doc/refman/8.0/en/server-system-variables.html#sysvar_default_authentication_plugin
 			var defaultAuthenticationPlugin string
-			if err := db.QueryRowContext(ctx, "SELECT @@default_authentication_plugin").Scan(&defaultAuthenticationPlugin); err != nil {
-				resp.Diagnostics.AddError("Failed to fetching @@default_authentication_plugin", err.Error())
-				return
+			err := db.QueryRowContext(ctx, "SELECT @@default_authentication_plugin").Scan(&defaultAuthenticationPlugin)
+			if err != nil {
+				// For MySQL 8.4+ where default_authentication_plugin is removed
+				// Default authentication plugin is caching_sha2_password
+				defaultAuthenticationPlugin = "caching_sha2_password"
+				tflog.Info(ctx, fmt.Sprintf("Using hardcoded default plugin for MySQL 8.4+: %s", defaultAuthenticationPlugin))
+			} else {
+				tflog.Info(ctx, fmt.Sprintf("default_authentication_plugin=%s", defaultAuthenticationPlugin))
 			}
-			tflog.Info(ctx, fmt.Sprintf("default_authentication_plugin=%s", defaultAuthenticationPlugin))
 			if plugin != defaultAuthenticationPlugin {
 				attributes := map[string]attr.Value{
 					"plugin":          types.StringValue(plugin),
